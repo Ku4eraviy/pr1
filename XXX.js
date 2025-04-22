6d648c0a4949b1d722a5f2a0496d96a07e7a73f9
Андрей Попов <popov_andrey2023@spobip-bel.ru>
2025-04-22 08:56:06 +0300

    готово

--- XXX.js
+++ XXX.js
@@ -2,6 +2,7 @@ const Error_menu_open = document.querySelector('.text_info'); //названия
 const close_btn = document.querySelector('.XX')
 const module = document.querySelector('.pramougol');
 const dark = document.querySelector('.body')
+const overlay = document.querySelector('.overlay');
 
 Error_menu_open.addEventListener('click', () => { //вызов меню по клику
     module.classList.add('open');
@@ -12,3 +13,22 @@ close_btn.addEventListener('click', () => { // закрытие меню
     module.classList.remove('open');
     dark.classList.remove('active')
 });
+
+// Получаем элементы DOM
+const toggleButton = document.getElementById('toggleButton');
+
+
+// Функция включения/отключения затемнения
+function toggleOverlay() {
+    if (overlay.style.display === 'none' || overlay.style.display === '') {
+        // Показываем затемнение
+        overlay.style.display = 'block';
+    } else {
+        // Скрываем затемнение
+        overlay.style.display = 'none';
+    }
+}
+
+// Добавляем обработчик события click на кнопку
+Error_menu_open.addEventListener('click', toggleOverlay);
+close_btn.addEventListener('click', toggleOverlay);
